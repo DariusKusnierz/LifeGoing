13c28186ec9a98e0814abf691ac17b89dff6cf27
DariusKusnierz <72936095+DariusKusnierz@users.noreply.github.com>
2023-09-25 23:07:19 +0200

    Added enemy cell's class

---
+++ Source/LifeGoing/CellEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Cell.h"
+#include "CellEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIFEGOING_API ACellEnemy : public ACell
+{
+	GENERATED_BODY()
+	
+};
